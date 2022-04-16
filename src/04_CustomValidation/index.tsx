217e665374ab310173af68209ca1ffa22c7b3831
안유진 <Eugenius1st@gmail.com>
2022-04-16 19:58:51 +0900

    study[#5]: customValidation & setValue

--- src/04_CustomValidation/index.tsx
+++ src/04_CustomValidation/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
-export default function ToDoList() {
+export default function CustomValidation() {
     interface IForm {
         email: string;
         firstName: string;
